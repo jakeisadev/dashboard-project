dac08c38017eda4ed287a2122daa65915bf88f0f
jakeisadev <starowicz@protonmail.com>
2022-07-26 15:38:28 -0400

    added timestamp scripts to click buttons and view different data

--- js/chart.js
+++ js/chart.js
@@ -55,3 +55,124 @@ let trafficOptions = {
     options: trafficOptions
   });
   
+  buttonsTrafficUl.addEventListener("click", function (e) {
+    const letter = document.querySelectorAll(".buttons-traffic-link");
+    for (let i = 0; i < letter.length; i++) {
+      letter[i].classList.remove("active");
+    }
+    if (event.target.tagName === "LI") {
+      let button = event.target;
+      button.className += " active";
+      const letterFound = button.classList;
+      for (let i = 0; i < letterFound.length; i++) {
+        if (letterFound[i] === "hourly") {
+          trafficChart.data.labels = [
+            "7am",
+            "8am",
+            "9am",
+            "10am",
+            "11am",
+            "12pm",
+            "1pm",
+            "2pm",
+            "3pm",
+            "4pm",
+            "5pm"
+          ];
+          trafficChart.data.datasets[0].data = [
+            20,
+            37,
+            45,
+            42,
+            30,
+            34,
+            46,
+            49,
+            35,
+            39,
+            29
+          ];
+          trafficChart.update();
+        } else if (letterFound[i] === "daily") {
+          trafficChart.data.labels = [
+            "Sun",
+            "Mon",
+            "Tue",
+            "Wed",
+            "Thu",
+            "Fri",
+            "Sat"
+          ];
+          trafficChart.data.datasets[0].data = [
+            380,
+            340,
+            320,
+            370,
+            340,
+            330,
+            300
+          ];
+          trafficChart.update();
+        } else if (letterFound[i] === "weekly") {
+          trafficChart.data.labels = [
+            "16-22",
+            "23-29",
+            "30-5",
+            "6-12",
+            "13-19",
+            "20-26",
+            "27-3",
+            "4-10",
+            "11-17",
+            "18-24",
+            "25-31"
+          ];
+          trafficChart.data.datasets[0].data = [
+            750,
+            1250,
+            1000,
+            1100,
+            1450,
+            1250,
+            1000,
+            1650,
+            2000,
+            1700,
+            2200
+          ];
+          trafficChart.update();
+        } else if (letterFound[i] === "monthly") {
+          trafficChart.data.labels = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+          ];
+          trafficChart.data.datasets[0].data = [
+            21750,
+            23250,
+            22000,
+            24800,
+            19450,
+            22250,
+            21000,
+            23000,
+            28000,
+            22700,
+            25200,
+            23000
+          ];
+          trafficChart.update();
+        }
+      }
+    }
+  });
+  //End Traffic Widget
